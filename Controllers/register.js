2037b2613fb15603ba969668a20772f2ffaa7d38
zeeshan1509 <zeeshan1509@users.noreply.github.com>
2020-06-10 23:22:07 +0500

    changes

--- Controllers/register.js
+++ Controllers/register.js
@@ -1,7 +1,7 @@
 const handleRegister = (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
-   if(!email || !name || !password){
-   return res.status(400).json('incorrect form submission');
+  if (!email || !name || !password) {
+    return res.status(400).json('incorrect form submission');
   }
   const hash = bcrypt.hashSync(password);
     db.transaction(trx => {
@@ -31,5 +31,4 @@ const handleRegister = (req, res, db, bcrypt) => {
 
 module.exports = {
   handleRegister: handleRegister
-
 };
