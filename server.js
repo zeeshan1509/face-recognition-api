e034fe63fdacdf8f8431a771302d5eeab3a4ed6f
zeeshan1509 <zeeshan1509@users.noreply.github.com>
2020-06-10 21:01:45 +0500

    changeee

--- server.js
+++ server.js
@@ -45,6 +45,6 @@ app.post('/imageurl',(req, res) => {image.handleApiCall(req,res,db)})
 
 
 
-app.listen(3000,()=>{
-	console.log('app is running port 3000')
+app.listen(process.env.PORT || 3000,()=>{
+	console.log(`app is running port ${process.env.PORT}`)
 })
